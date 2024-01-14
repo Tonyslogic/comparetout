d8da84f75bd1446b353ba4c5d4e6ee473d5bd49f
Tony Finnerty <tonysfreecode@gmail.com>
2024-01-14 08:48:54 +0000

    Copyright update

--- app/src/androidTest/java/com/tfcode/comparetout/model/ScenarioDAOTest.java
+++ app/src/androidTest/java/com/tfcode/comparetout/model/ScenarioDAOTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023. Tony Finnerty
+ * Copyright (c) 2023-2024. Tony Finnerty
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
