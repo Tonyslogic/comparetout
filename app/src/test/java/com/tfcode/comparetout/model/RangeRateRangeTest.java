da6c297274fb23216546aa5db641e3ed548fc5b5
tonyslogic <tonysfreecode@gmail.com>
2024-06-22 09:27:25 +0100

    PricePlan updates, cleanup not needed file

--- app/src/test/java/com/tfcode/comparetout/model/RangeRateRangeTest.java
+++ app/src/test/java/com/tfcode/comparetout/model/RangeRateRangeTest.java
@@ -16,17 +16,20 @@
 
 package com.tfcode.comparetout.model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import com.tfcode.comparetout.model.priceplan.DoubleHolder;
-import com.tfcode.comparetout.model.priceplan.HourlyRateRange;
+import com.tfcode.comparetout.model.priceplan.MinuteRateRange;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RangeRateRangeTest {
 
-    HourlyRateRange testObject;
+    MinuteRateRange testObject;
     DoubleHolder dh;
 
     @Before
@@ -39,31 +42,36 @@ public class RangeRateRangeTest {
         dh.update(17, 19, 30.0);
         dh.update(19, 23, 20.0);
         dh.update(23,24,10.0);
-        testObject = new HourlyRateRange(dh);
+        testObject = MinuteRateRange.fromHours(dh);
     }
 
     @Test
     public void lookup() {
         assertEquals(10.0, testObject.lookup(0), 0);
-        assertEquals(10.0, testObject.lookup(1), 0);
-        assertEquals(8.0, testObject.lookup(2), 0);
-        assertEquals(30.0, testObject.lookup(18), 0);
-        assertEquals(10.0, testObject.lookup(24), 0);
+        assertEquals(10.0, testObject.lookup(60), 0);
+        assertEquals(8.0, testObject.lookup(2 * 60), 0);
+        assertEquals(30.0, testObject.lookup(18 * 60), 0);
+        assertEquals(10.0, testObject.lookup(24 * 60), 0);
     }
 
     @Test
     public void getDoubleHolder() {
+        DoubleHolder dh = new DoubleHolder();
+        List<Double> doubles = new ArrayList<>();
+        for (int i = 0; i < 25; i++) doubles.add(testObject.lookup(i * 60));
+        dh.doubles = doubles;
+
         assertEquals(
                 "[10.0, 10.0, 8.0, 8.0, 8.0, 8.0, 10.0, 10.0, 20.0, 20.0, 20.0, 20.0, " +
                         "20.0, 20.0, 20.0, 20.0, 20.0, 30.0, 30.0, 20.0, 20.0, 20.0, 20.0, 10.0, 10.0]",
-                testObject.getDoubleHolder().toString());
+                dh.toString());
     }
 
     @Test
     public void getRates() {
         assertEquals(7, testObject.getRates().size());
         dh.update(17,19,20.0);
-        testObject = new HourlyRateRange(dh);
+        testObject = MinuteRateRange.fromHours(dh);
         assertEquals(5, testObject.getRates().size());
     }
 }
