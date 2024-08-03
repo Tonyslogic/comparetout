c88651594928084e076ffb6784c632446312c620
tonyslogic <tonysfreecode@gmail.com>
2024-08-03 11:14:15 +0100

    Updating test secrets

---
+++ app/src/test/java/com/tfcode/comparetout/importers/TestSecrets.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023-2024. Tony Finnerty
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.tfcode.comparetout.importers;
+
+public class TestSecrets {
+    // The serial number of the AlphaESS system to use for testing
+    public static final String SERIAL = "AL************";
+
+    // The AppID or Developer ID from open.alphaess.com (registration required
+    public static final String APPID = "alpha****************";
+
+    // The AppSecret or Developer Secret from open.alphaess.com (registration required
+    public static final String SECRET = "********************************";
+
+    public static final String ESBN_USER = "user@email.com";
+
+    public static final String ESBN_PASSWORD = "password";
+
+    public static final String ESBN_MPRN = "12345678910";
+
+    public static final String HA_TOKEN = ";ashgoaihg[opiahgoihapogihaps[oigha";
+}
